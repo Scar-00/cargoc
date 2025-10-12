b8cc05a7d1d6b436def8352c58afc54873f6a410
Konstantin Goldmann <konstantingoldmann@web.de>
2025-10-13 00:19:21 +0200

    added better build.lua

--- build.lua
+++ build.lua
@@ -39,15 +39,33 @@
 
 ---@param build Build
 return function (build)
+    local tool_chain = "Clang";
+    local warnings = { "Error", "Pedantic", "All", "Extra" };
+    local no_warnings = { "DeprecatedDeclarations" };
+
+    if tool_chain == "Msvc" then
+        warnings = {};
+        no_warnings = {};
+    end
+
     local main = build:add_binary({
-        tool_chain = build:default_toolchain(),
+        tool_chain = tool_chain,
         opt_level = build:default_opt_level(),
         files = {
-            "main.c"
+            "src/main.c"
         },
+        src_dir = "src",
         output = "main",
+        includes = {
+            "../../learning/core/"
+        },
+        args = {
+            warnings = warnings,
+            no_warnings = no_warnings,
+        }
     });
-    local exe = main:build();
-    local output = build:install(exe);
-    print(output);
+    local exe = main:build_and_install();
+    if exe and build:wants_run() then
+        build:run(exe, { "bar", "baz" });
+    end
 end
